70e8126c41aa05a6724785d8465e4f190646bc8a
Chloe <chloe@ChloedeMacBook-Air.local>
2022-10-09 11:53:44 -0700

    lab4

---
+++ Lab4/main.java
@@ -0,0 +1,73 @@
+/**
+ *  Assignment 3
+ *  HsiaoChing Chen
+ *  002749406
+ *  Section 08
+ */
+
+import java.util.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        // Create an object of the weightedgrade class
+        weightedgrade wg = new weightedgrade();
+
+        // Get user input of Number of Assignments
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Please Enter Number of Assignments: ");
+        String arraySize = user_input.nextLine();
+
+        // Get user input of Point Total
+        System.out.println("Please enter Point Total of " + arraySize + " Assignments: ");
+        String points = user_input.nextLine();
+        // Remove all whitespace from user input
+        String newPoints = points.replaceAll(" ", "");
+        // Create a new list to storage the Total Point
+        ArrayList<String> point = new ArrayList<>(Arrays.asList(newPoints.split(",")));
+        // Check if input is not grater than arraySize
+        if (point.size() > Integer.valueOf(arraySize)){
+            System.out.println("The number of input Point total can't over " + arraySize + ". Please start over.");
+            System.exit(0);
+        }
+
+        // Get user input of Earned Point
+        System.out.println("Please enter Earned Point of " + arraySize + " Assignments: ");
+        String earns = user_input.nextLine();
+        // Remove all whitespace from user input
+        String newEarns = earns.replaceAll(" ", "");
+        // Create a new list to storage the Earned Point
+        ArrayList<String> earned = new ArrayList<>(Arrays.asList(newEarns.split(",")));
+        // Check if input is not grater than arraySize
+        if (earned.size() > Integer.valueOf(arraySize)){
+            System.out.println("The number of input Earned Point can't over " + arraySize + ". Please start over.");
+            System.exit(0);
+        }
+
+        // Get user input of Assigment Percentage
+        System.out.println("Please enter Assigment Percentage(%) of "+arraySize+" Assignments: ");
+        String percents = user_input.nextLine();
+        // Remove all whitespace from user input
+        String newPercents = percents.replaceAll(" ", "");
+        // Create a new list to storage the Assigment Percentage
+        ArrayList<String> percent = new ArrayList<>(Arrays.asList(newPercents.split(",")));
+        // Check if input is not grater than arraySize
+        if (percent.size() > Integer.valueOf(arraySize)){
+            System.out.println("The number of Assigment Percentage can't over " + arraySize + ". Please start over.");
+            System.exit(0);
+        }
+
+        // Calculate Total Weighted Grade that using for loop
+        for(int i = 0; i < Integer.valueOf(arraySize); i ++) {
+            wg.setTotalGrade(Integer.valueOf(point.get(i)),
+                            Integer.valueOf(earned.get(i)),
+                            Integer.valueOf(percent.get(i)));
+        }
+
+        // Show the final result of Total Grade and Letter Grade
+        System.out.println("======== Final Result ========");
+        System.out.println("Total Grade = " + wg.getTotalGrade());
+        System.out.println("Letter Grade = " + wg.LetterGarde(wg.getTotalGrade()));
+    }
+}
---
+++ Lab4/weightedgrade.java
@@ -0,0 +1,73 @@
+/**
+ *  Assignment 3
+ *  HsiaoChing Chen
+ *  002749406
+ *  Section 08
+ */
+
+public class weightedgrade {
+
+    private int pointTotal;
+    private int earnedPoints;
+    private double assignmentPercent;
+    // Initialize Total Weighted Grade as 0
+    private double totalWeightedGrade = 0;
+
+    // Set the Total Point
+    public void setPointTotal(int pointTotal){
+        this.pointTotal = pointTotal;
+    }
+
+    // Get the Total Point
+    public int getPointTotal(){
+        return pointTotal;
+    }
+
+    // Set the Earned Point
+    public void setEarnedPoints(int earnedPoints){
+        this.earnedPoints = earnedPoints;
+    }
+
+    // Get the Earned Point
+    public int getEarnedPoints(){
+        return earnedPoints;
+    }
+
+    // Set the Assigment Percentage
+    public void setAssigmentPercentage(double assignmentPercent){
+        this.assignmentPercent = assignmentPercent;
+    }
+
+    // Get the Assigment Percentage
+    public double getAssigmentPercentage(){
+        return assignmentPercent;
+    }
+
+    // Set the Total Weighted Grade
+    public void setTotalGrade(int pointTotal, int earnedPoints, double assignmentPercent) {
+        this.totalWeightedGrade = totalWeightedGrade+((float)earnedPoints/pointTotal)*assignmentPercent;
+    }
+
+    // Get the Total Weighted Grade
+    public double getTotalGrade(){
+        return totalWeightedGrade;
+    }
+
+    // Get the result of Letter Grade by Total Weighted Grade
+    public char LetterGarde(double totalWeightedGrade){
+        char grade;
+
+        if (totalWeightedGrade >= 90){
+            grade = 'A';
+        } else if (totalWeightedGrade>= 80) {
+            grade = 'B';
+        } else if (totalWeightedGrade>= 70) {
+            grade = 'C';
+        } else if (totalWeightedGrade>= 60) {
+            grade = 'D';
+        }else{
+            grade = 'F';
+        }
+        return grade;
+    }
+}
